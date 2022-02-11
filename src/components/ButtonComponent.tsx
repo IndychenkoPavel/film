194b27fa4b51bfcc600a540c9daa4ee3d08bacd2
IndychenkoPavel <pulizaf@gmail.com>
2022-02-11 19:14:58 +0200

    first-brunch

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ButtonComponent = () => {
+  return (
+    <div className='button-block'>
+        <button className='btn-edit'>edit</button>
+        <button className='btn-delete'>delete</button>
+    </div>
+  )
+}
+
+export default ButtonComponent
